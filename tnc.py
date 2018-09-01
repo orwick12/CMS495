394ec8cefd4ed9fd8322aa8e195b8fd77f36b04b
Yrume Fernandez <yrume.fernandez@gmail.com>
2018-09-01 18:49:21 -0400

    Add files via upload

--- tnc.py
+++ tnc.py
@@ -14,6 +14,15 @@
 # import required modules for webscraping and html parsing
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
+import sys
+
+# Build Database
+con = sqlite3.connect('tnc.db')
+with con:
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS NewsArticle")
+    cur.execute("CREATE TABLE NewsArticle(Id INT, Name TEXT, Count INT)")
 
 # The News Counter Webscraper
 def tncWebscraper():
@@ -31,14 +40,22 @@ def tncWebscraper():
     textContent = []
     i = 0
     for p in page_content.find_all("article"):
-        print (i)
+        #print (i)
         paragraphs = page_content.find_all("article")[i].text
         textContent.append(paragraphs)
-        print (paragraphs)
+        #print (paragraphs)
+        cur.execute("INSERT INTO NewsArticle VALUES(?,?,?)", (i, paragraphs, 1))
         i = i+1
+
+def dbRetrieve():
+    cur.execute("SELECT * FROM NewsArticle")
+    data=cur.fetchall()
+    for line in data:
+        print(line)
 
 def main():
     tncWebscraper()
+    dbRetrieve()
 
 if __name__=="__main__":
     main()
