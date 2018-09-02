99fc22cfb54347b63412c4f8136a5a45314289bf
Yrume Fernandez <yrume.fernandez@gmail.com>
2018-09-02 12:37:05 -0400

    Add files via upload

--- src/back_end/tnc.py
+++ src/back_end/tnc.py
@@ -23,30 +23,30 @@ con = sqlite3.connect('tnc.db')
 with con:
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS NewsArticle")
-    cur.execute("CREATE TABLE NewsArticle(Id INT, Name TEXT, Count INT)")
+    cur.execute("CREATE TABLE NewsArticle(Id TEXT, Number INT, Name TEXT, Count INT)")
 
 # The News Counter Webscraper
 def tncWebscraper():
     
     # create variables
-    web_page = 'https://www.foxnews.com'
+    web_list = ['https://www.foxnews.com', 'https://www.usatoday.com']
 
+    for web_page in web_list:
+        # set get request for html
+        page_response = requests.get(web_page, timeout=5)
+        print (page_response)
 
-    # set get request for html
-    page_response = requests.get(web_page, timeout=5)
-    print (page_response)
+        page_content = BeautifulSoup(page_response.content, "html.parser")
 
-    page_content = BeautifulSoup(page_response.content, "html.parser")
-
-    textContent = []
-    i = 0
-    for p in page_content.find_all("article"):
-        #print (i)
-        paragraphs = page_content.find_all("article")[i].text
-        textContent.append(paragraphs)
-        #print (paragraphs)
-        cur.execute("INSERT INTO NewsArticle VALUES(?,?,?)", (i, paragraphs, 1))
-        i = i+1
+        textContent = []
+        i = 0
+        for p in page_content.find_all("article"):
+            #print (i)
+            paragraphs = page_content.find_all("article")[i].text
+            textContent.append(paragraphs)
+            #print (paragraphs)
+            cur.execute("INSERT INTO NewsArticle VALUES(?,?,?,?)", (web_page, i, paragraphs, 1))
+            i = i+1
 
 def dbRetrieve():
     cur.execute("SELECT * FROM NewsArticle")
@@ -54,9 +54,23 @@ def dbRetrieve():
     for line in data:
         print(line)
 
+def compareArticle():
+    wordcount = 0
+    cur.execute("SELECT * FROM NewsArticle")
+    words=cur.fetchall()
+    for line in words:
+        site, id, title, count = line
+        word = title.split()
+        #print(type(word))
+        for x in word:
+            if x in words:
+                wordcount = wordcount + 1
+                print(wordcount)
+
 def main():
     tncWebscraper()
     dbRetrieve()
+    compareArticle()
 
 if __name__=="__main__":
     main()
