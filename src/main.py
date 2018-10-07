6a25f15c419a12abc34d5629d0631f976653ee3a
Andrew Christiano <ajchristiano91@gmail.com>
2018-10-07 16:59:12 -0400

    uploading user guide

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from front_end.Web import Web
 class Main(object):
     def __init__(self):
         self.db = DB()
-        self.scraper = Scraper(["https://cnn.com/", "https://foxnews.com/", "https://washintonpost.com/", "https://www.reuters.com/"], self.db)
+        self.scraper = Scraper(["https://cnn.com/", "https://foxnews.com/"], self.db)
         self.web = Web(self.scraper, self.db)
 
 
