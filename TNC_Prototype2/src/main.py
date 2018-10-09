793b0705a5fdb2892cdee413f2da0cbbb4ff8432
Yrume Fernandez <yrume.fernandez@gmail.com>
2018-10-09 08:45:05 -0400

    protype2
    protype2

---
+++ TNC_Prototype2/src/main.py
@@ -0,0 +1,22 @@
+from back_end.DB import DB
+from back_end.Scraper import Scraper
+from front_end.Web import Web
+from back_end.TNC import TNC
+from threading import Timer
+import datetime as dt
+
+
+class Main(object):
+    def __init__(self):
+        #nextDay = dt.datetime.now() + dt.timedelta(days=1)
+        #dateString = nextDay.strftime('%d-%m-%Y') + " 01-00-00"
+        #newDate = nextDay.strptime(dateString, '%d-%m-%Y %H-%M-%S')
+        #delay = (newDate - dt.datetime.now()).total_seconds()
+        self.tnc = TNC()
+        self.db = self.tnc.connectDB()
+        self.tnc.tncWebscraper()
+        self.web = Web(self.tnc, self.db )
+        #Timer(delay, self.tnc.tncWebscraper, ()).start()
+
+
+
